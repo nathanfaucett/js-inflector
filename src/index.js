aa896b8e41f5da86ea931b9ea0b8a61b5f43bb4a
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 12:23:59 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -65,65 +65,41 @@ InflectorPrototype.irregular = function(singular, plural) {
 };
 
 InflectorPrototype.pluralize = function(word) {
-    var plurals = this.plurals,
-        result = word,
-        i = plurals.length,
-        pattern;
-
-    if (indexOf(this.uncountables, word.toLowerCase()) !== -1) {
-        return word;
-    }
-
-    while (i--) {
-        pattern = plurals[i];
-
-        if ((result = replace(word, pattern[0], pattern[1]))) {
-            return result;
-        }
-    }
-
-    return word;
+    return replace(this.uncountables, this.plurals, word);
 };
 
 InflectorPrototype.isPlural = function(word) {
-
     return this.singularize(word) !== word;
 };
 
 InflectorPrototype.is_plural = InflectorPrototype.isPlural;
 
 InflectorPrototype.singularize = function(word) {
-    var singulars = this.singulars,
-        result = word,
-        i = singulars.length,
-        pattern;
-
-    if (indexOf(this.uncountables, word.toLowerCase()) !== -1) {
-        return word;
-    }
-
-    while (i--) {
-        pattern = singulars[i];
-
-        if ((result = replace(word, pattern[0], pattern[1]))) {
-            return result;
-        }
-    }
-
-    return word;
+    return replace(this.uncountables, this.singulars, word);
 };
 
 InflectorPrototype.isSingular = function(word) {
-
     return this.pluralize(word) !== word;
 };
 
 InflectorPrototype.is_singular = InflectorPrototype.isSingular;
 
-function replace(word, rule, replacement) {
-    if (rule.test(word)) {
-        return word.replace(rule, replacement);
+function replace(uncountables, rules, word) {
+    var i = rules.length,
+        pattern, rule;
+
+    if (indexOf(uncountables, word.toLowerCase()) !== -1) {
+        return word;
     } else {
-        return false;
+        while (i--) {
+            pattern = rules[i];
+            rule = pattern[0];
+
+            if (rule.test(word)) {
+                return word.replace(rule, pattern[1]);
+            }
+        }
+
+        return word;
     }
 }
